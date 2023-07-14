89d926742a8bdee863bf0296a4213a6332c36241
Brono <brunobrunelli1995@gmail.com>
2023-07-14 16:41:49 -0300

    PHP

---
+++ app_help_desk/valida_login.php
@@ -0,0 +1,48 @@
+<?php
+
+    //variavel que verifica se a autenticação foi realizada
+    $usuario_autenticado = false;
+
+    //usuarios do sistema
+
+    $usuarios_app = array(
+        array('email' => 'adm@adm.com.br', 'senha' => '123456'),
+        array('email' => 'user@adm.com.br', 'senha' => 'abcd'),
+    );
+
+    /*
+    echo '<pre>';
+    print_r($usuarios_app);
+    echo '</pre>';
+    */
+
+    foreach($usuarios_app as $user) {
+        if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
+            $usuario_autenticado = true;
+        }
+    }
+
+    if($usuario_autenticado) {
+        echo 'Usuário autentiado';
+    } else {
+        header('Location: index.php?login=erro');
+    }
+
+    /*
+    print_r($_GET);
+
+    echo '<br>';
+
+    echo $_GET['email'];
+    echo '<br>';
+    echo $_GET['senha'];
+    */
+    /*
+    print_r($_POST);
+    echo '<br>';
+
+    echo $_POST['email'];
+    echo '<br>';
+    echo $_POST['senha'];
+    */
+?>
