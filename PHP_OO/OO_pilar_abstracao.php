8be937585b1395af1f66bce603943bed6570ccd0
Brono <brunobrunelli1995@gmail.com>
2023-07-20 08:29:55 -0300

    PHP

--- PHP_OO/OO_pilar_abstracao.php
+++ PHP_OO/OO_pilar_abstracao.php
@@ -3,31 +3,65 @@
     //modelo
     class Funcionario {
         //atributos
-        public $nome = 'José';
-        public $telefone = '99 99999-9999';
-        public $numFilhos = 2;
+        public $nome = null;
+        public $telefone = null;
+        public $numFilhos = null;
+        public $cargo = null;
+        public $salario = null;
+
+        //getters setters (overloading / sobrecarregar)
+        function __set($atributo, $valor) {
+            $this->$atributo = $valor;
+        }
+
+        function __get($atributo) {
+            return $this->$atributo;
+        }
+
+
+
+        /*
+        function setNome($nome) {
+            $this->nome = $nome;
+        }
+
+        function setNumFilhos ($numFilhos) {
+            $this->numFilhos = $numFilhos;
+        }
+
+        function getNome() {
+            return $this->nome;
+        }
+
+        function getNumFilhos() {
+            return $this->numFilhos;
+        }
+        */
         
         //métodos
         function resumirCadFunc() {
-            return "$this->nome possui $this->numFilhos filho(s)";
+            return $this->__get('nome') . " possui " . $this->__get('numFilhos') . " filho(s)";
         }
 
         function modificarNumFilhos($numFilhos) {
             //afetar um atributo do objeto
             $this->numFilhos = $numFilhos;
         }
+        
 
     }
 
     $y = new Funcionario();
+    $y->__set('nome', 'José');
+    $y->__set('numFilhos', 2);
     echo $y->resumirCadFunc();
+    //echo $y->__get('nome') . ' possui ' . $y->__get('numFilhos') . ' filho(s)';
+
     echo '<br>';
-    $y->modificarNumFilhos(3);
-    echo $y->resumirCadFunc();
-    echo '<hr>';
 
     $x = new Funcionario();
+    $x->__set('nome', 'Maria');
+    $x->__set('numFilhos', 0);
     echo $x->resumirCadFunc();
-    echo '<br>';
-    $x->modificarNumFilhos(1);
-    echo $x->resumirCadFunc();
+    //echo $x->__get('nome') . ' possui ' . $x->__get('numFilhos') . ' filho(s)';
+    
